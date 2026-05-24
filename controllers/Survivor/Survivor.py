681beee8f664a25314e78b9e9f4ccba3a45a9ae9
CobberH <162146732+CobberH@users.noreply.github.com>
2026-05-24 18:46:40 +1000

    overwrite survivor

--- controllers/Survivor/Survivor.py
+++ controllers/Survivor/Survivor.py
@@ -3,6 +3,7 @@ import random
 
 robot = Supervisor()
 timestep = int(robot.getBasicTimeStep())
+<<<<<<< Updated upstream
 
 print("Supervisor running")
 
@@ -15,8 +16,24 @@ positions = [
     [0.3, 0.45, 0],
     [-0.4, -0.45, 0]
     
-]
+=======
+print("Supervisor running")
 
+positions = [
+
+    [0.25, 0.25, 0],
+
+    [-0.25, 0.25, 0],
+
+    [0.25, -0.25, 0],
+
+    [-0.25, -0.25, 0],  
+
+>>>>>>> Stashed changes
+]
+# Survivors
+
+<<<<<<< Updated upstream
 # Survivors
 survivor1 = robot.getFromDef("Survivor")
 survivor2 = robot.getFromDef("Survivor2")
@@ -33,4 +50,36 @@ survivor3.getField("translation").setSFVec3f(positions[2])
 print("Survivors Worked?")
 
 while robot.step(timestep) != -1:
+=======
+survivor1 = robot.getFromDef("Survivor")
+
+survivor2 = robot.getFromDef("Survivor2")
+
+survivor3 = robot.getFromDef("Survivor3")
+
+ 
+
+ 
+
+# random position
+
+random.shuffle(positions)
+
+ 
+
+survivor1.getField("translation").setSFVec3f(positions[0])
+
+survivor2.getField("translation").setSFVec3f(positions[1])
+
+survivor3.getField("translation").setSFVec3f(positions[2])
+
+ 
+
+print("Survivors Worked?")
+
+ 
+
+while robot.step(timestep) != -1:
+
+>>>>>>> Stashed changes
     pass
